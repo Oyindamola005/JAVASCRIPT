8c00f596cfe5af47621993af0aff6700fc2b5e16
oyindamola005 <damolaoyin005@gmail.com>
2025-04-18 14:02:07 +0100

    Fix: Add content to JS file

--- BASIC/excercise1.js
+++ BASIC/excercise1.js
@@ -1,1 +1,10 @@
+let userinput = prompt("Enter a number");
+number = Number(userinput)
+if (number % 2 ==0)
+    {alert
+        (number + " is an even number");
+    }
+    else { 
+        alert(number + " is an odd number");}
+
 
