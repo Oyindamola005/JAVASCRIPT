e0af2457430e7e947afdbf33b9e495ad977398c0
oyindamola005 <damolaoyin005@gmail.com>
2025-04-18 15:56:47 +0100

    WIP: Save changes before pulling

--- BASIC/excercise5.js
+++ BASIC/excercise5.js
@@ -1,1 +1,6 @@
-
+let firstnumber= prompt("enter number");
+firstnumber = parseInt(firstnumber);
+console.log('multiplation table for ${firstnumber}');
+for (let n=1; n <=12; n++) {
+    console.log ('${firstnumber} x ${n}= ${number*n}');
+}
