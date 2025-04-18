2110465abf1d637b2ea7e69de757403833421450
oyindamola005 <damolaoyin005@gmail.com>
2025-04-18 14:51:14 +0100

    WIP: Save changes before pulling

--- BASIC/excercise2.js
+++ BASIC/excercise2.js
@@ -1,1 +1,24 @@
-
+let firstnumber = prompt("enter first number");
+let operator =prompt("enter operator (+, -, *, /)")
+let secondnumber = prompt("enter second number");
+firstnumber = parseFloat(firstnumber);
+secondnumber = parseFloat(secondnumber);
+let result;
+if (operator == "+") {
+    result = firstnumber + secondnumber;
+    alert("Result is: " + result);
+}
+else if (operator == "-") {
+    result = firstnumber - secondnumber;
+    alert("Result is: " + result);
+    
+}
+else if (operator == "*") {
+    result = firstnumber * secondnumber;
+    alert("Result is: " + result);
+}
+else if (operator == "/") {
+    result = firstnumber / secondnumber;
+    alert("Result is: " + result);
+}
+else { alert ("invalide operator");}
