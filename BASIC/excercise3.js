1eb577bbab8336f5a128b666bc03d779d660ce36
oyindamola005 <damolaoyin005@gmail.com>
2025-04-20 10:19:08 +0100

    WIP: Save changes before pulling

--- BASIC/excercise3.js
+++ BASIC/excercise3.js
@@ -1,7 +1,8 @@
 let Age = prompt ("enter your name");
 if (Age >= 18){
-    alert("Voter's is eligible");
+    alert("Congratulations, you are eligible, Proceed to cast your vote");
 }
 else {
-    alert("Voter's is not Eligible");
+    let RemaingYears = 18 - Age
+    alert("Sorry, you are not eligible, come back in " + RemaingYears + "  years");
 }
