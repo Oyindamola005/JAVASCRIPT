8ed366bfe5d5628f8b823b566a635dce8d983bcd
oyindamola005 <damolaoyin005@gmail.com>
2025-04-20 11:59:36 +0100

    saved

--- BASIC/excercise7.js
+++ BASIC/excercise7.js
@@ -1,1 +1,10 @@
+let Randomnumber =prompt("Guess the number");
+Randomnumber =parseInt(Randomnumber);
+let  newrandom = Math.floor(Math.random()*5)+1;
+if (Randomnumber==newrandom){
+    alert("You are a star🌟🌟🌟🌟🌟🌟");
+}
+else{
+    alert("Oops, you are almost there ↕️↕️↕️↕️ but the correct number is " + newrandom);
+}
 
