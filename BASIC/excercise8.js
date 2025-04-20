512f5b86ab4d08c8ea8b5853afeef933fdeed21a
oyindamola005 <damolaoyin005@gmail.com>
2025-04-20 12:30:48 +0100

    save

--- BASIC/excercise8.js
+++ BASIC/excercise8.js
@@ -1,1 +1,14 @@
-
+for(let n=1; n<=15; n++){
+    if(n%3==0){
+        console.log("FIZZ");
+    }
+    else if (n%5==0){
+        console.log("BUZZ");
+    }
+    else if ( n%5===0 && n%3==0){
+        console.log("FIZZBUZZ");
+    }
+    else{
+        console.log(n);
+    }
+}
