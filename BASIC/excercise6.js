d8ff8c8b2c509a2d0fedce877ce0a0da18667c96
oyindamola005 <damolaoyin005@gmail.com>
2025-04-20 11:36:09 +0100

    commit change

--- BASIC/excercise6.js
+++ BASIC/excercise6.js
@@ -1,1 +1,14 @@
-
+let Score = prompt("Enter your score");
+Score = parseFloat(Score);
+if ( Score >=70){
+    alert("Congratuations, you are a unicorn🦄");
+}
+else if (Score >=50 && Score < 69){
+    alert("Keep the fire burning 🔥🔥, you are do better next time");
+}
+else if (Score>=40 && Score <50){
+    alert("You need improvement 🤌🤌, Keep believing in your self");
+}
+else {
+    alert("You need serious Punishment 😠💢🤬");
+}
