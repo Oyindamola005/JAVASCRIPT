615ad3ba6a98bf769b7dfec1f354af9a68637be6
oyindamola005 <damolaoyin005@gmail.com>
2025-05-02 16:39:39 +0100

    coomit

--- Toggle/script.js
+++ Toggle/script.js
@@ -1,1 +1,8 @@
+const togglebtn = document.querySelector("toggle");
+const switchbtn = document.getElementById("switch");
+togglebtn.addEventListener ("click", function(){
+    document.body.classList.toggle("dark")
+}
+)
+
 
