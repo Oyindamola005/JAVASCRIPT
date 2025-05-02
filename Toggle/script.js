433038c8de42b7267436a39ce8e3ec4223ecf037
oyindamola005 <damolaoyin005@gmail.com>
2025-05-02 17:08:05 +0100

    commit

--- Toggle/script.js
+++ Toggle/script.js
@@ -1,4 +1,4 @@
-const toggle = document.querySelector("toggle");
+const toggle = document.querySelector(".toggle");
 toggle.addEventListener ("click", function (){
     document.body.classList.toggle("dark");
 }
