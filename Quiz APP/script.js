fbef9d41543720ae8184f96584511bbe405dda7b
oyindamola005 <damolaoyin005@gmail.com>
2025-05-04 10:02:11 +0100

    commit

--- Quiz APP/script.js
+++ Quiz APP/script.js
@@ -1,33 +1,34 @@
 const questions = [
   {
-      question1: "what is thw capital of Lagos State?",
-      answer1: ["Abuja" , "Abeokuta" , "Ikeja", "Ibadan"],
+      question: "what is thw capital of Lagos State?",
+      answer: ["Abuja" , "Abeokuta" , "Ikeja", "Ibadan"],
       correct:2
   },
   {
-      question2: "When did Nigeria receive her Independence?",
-      answer2: ["1960" , "1953" , "1900", "1920"],
+      question: "When did Nigeria receive her Independence?",
+      answer: ["1960" , "1953" , "1900", "1920"],
       correct:0
   },
   {
-      question3: "What is the captal of Abuja?",
-      answer3: ["Lagos","Jos","FCT", "Ekiti"],
+      question: "What is the captal of Abuja?",
+      answer: ["Lagos","Jos","FCT", "Ekiti"],
       correct:2
   },
   {
-      question4: "what is thw capital of Lagos State?",
-      answer4: ["Abuja" , "Abeokuta" , "Ikeja", "Ibadan"],
+      question: "what is thw capital of Lagos State?",
+      answer: ["Abuja" , "Abeokuta" , "Ikeja", "Ibadan"],
       correct:2
   },
   
 ];
-let firstQeustn=question1;
+let firstQeustn=0;
 let score =0;
-let firstAnswer = answer1;
+let firstAnswer = [];
 const question1 = document.getElementById.innerHTML("question");
 const answer1 =document.getElementById.innerHTML("answer");
 const nextButton = document.getElementById("next");
 const previousButton = document.getElementById ("previous");
+
 
 nextButton.addEventListener("click", function () {
 
