649a8e897fdba62c9513ec7a3c3021cfc402c8c5
oyindamola005 <damolaoyin005@gmail.com>
2025-05-10 11:36:47 +0100

    commit

--- Student Profile Generator/script.js
+++ Student Profile Generator/script.js
@@ -1,1 +1,33 @@
-
+const students = [
+    {firstName: "Abiola",
+    lastName: "Abimbola",
+    age:20,
+    score: 60,
+    courses: ["cse232","mee212",]
+},
+    {firstName: "Oyindamola",
+        lastName: "Odunayo",
+        age:21,
+        score: 61,
+        courses: ["mth204","cse212"]
+        },
+        {firstName: "Oluwafemi",
+            lastName: "Adegoke",
+            age:22,
+            score: 62,
+            courses: ["cse232","cse212"]
+            },
+            {firstName: "Abayomi",
+                lastName: "Gabriel",
+                age:23,
+                score: 63,
+                courses: ["mee212","mth204",],
+                },
+]
+const [thirdStudent] = students;
+const {firstName, lastName, age , score , courses}= thirdStudent;
+console.log(firstName);
+console.log(lastName);
+console.log(age);
+console.log(score);
+console.log(courses);
